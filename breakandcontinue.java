8e3476bc8551f59ccabddcc72ebb5fb7c2c23305
Abhishek Yergude <112966485+abhii019@users.noreply.github.com>
2023-09-02 15:59:00 +0530

    ..

---
+++ breakandcontinue.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public interface breakandcontinue {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+
+        while (true){
+             System.out.println("entre ythe nlo");
+            n =sc.nextInt();
+           
+
+            if(n!=0){
+                System.out.println("you eneterd the no"+n);
+                continue;
+            }
+            else{
+                break;
+            }
+        }
+    }
+}
