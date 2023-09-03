22366523f40ca70b7eeb3c83c994ec7e4cc73c4a
Abhishek Yergude <112966485+abhii019@users.noreply.github.com>
2023-09-03 10:39:22 +0530

    ..

--- enhanceloop.java
+++ enhanceloop.java
@@ -1,16 +1,17 @@
 public class enhanceloop {
     public static void main(String[] args) {
-        int a[]={2,3,4,5,6,7,8,9,0};
-        for(int t:a){
+        int a[] = { 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+        for (int t : a) {
             System.out.println(t);
         }
-      
+
     }
 }
- class  enhanceloop{
+
+class enhanceloop {
     public static void main(String[] args) {
-        String language[]={"a","b"};
-        for(String a:language){
+        String language[] = { "a", "b", "c", "d", "E" };
+        for (String a : language) {
             System.out.println(a);
         }
     }
