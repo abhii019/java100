cec5b2192545695a84731833e8c8cd8a533995aa
Abhishek Yergude <112966485+abhii019@users.noreply.github.com>
2023-09-06 10:13:56 +0530

    .

--- alphabet.java
+++ alphabet.java
@@ -2,6 +2,7 @@ public class alphabet {
     public static void main(String[] args) {
         
         char ch;
+        System.out.println("the alphabet are");
         for (ch='a';ch<='z';ch++){
             if(ch=='l'){
         continue;
