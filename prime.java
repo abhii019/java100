54dd67861b5fca3a54ed8a245e8f6280b5127b44
Abhishek Yergude <112966485+abhii019@users.noreply.github.com>
2023-09-11 21:21:46 +0530

    ..

---
+++ prime.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class prime {
+    public static void main(String[] args) {
+        int n, status =1, num =3;
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("enetr the numbers of prime no u want");
+        n= in.nextInt();
+        if (n>=1){
+            System.out.println("first" +n+"prime numnbers are:-");
+            System.out.println(2);
+        }for 
+        for (int count =2; count<=n;){
+
+        }
+    }
+
+}
