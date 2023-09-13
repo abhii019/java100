b7c15304a7227cde8d87b663e49ad96a5fa10765
Abhishek Yergude <112966485+abhii019@users.noreply.github.com>
2023-09-13 08:10:41 +0530

    .

--- prime.java
+++ prime.java
@@ -7,7 +7,7 @@ public class prime {
         System.out.println("enetr the numbers of prime no u want");
         n= in.nextInt();
         if (n>=1){
-            System.out.println("first" +n+"prime numnbers are:-");
+            System.out.println("first" +n+"prime numnbers are:");
             System.out.println(2);
         }for 
         for (int count =2; count<=n;){
