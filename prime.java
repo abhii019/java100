b0f05979f65f0ee98e8e34ee137ed112d0469e54
Abhishek Yergude <112966485+abhii019@users.noreply.github.com>
2023-09-19 22:42:25 +0530

    .

--- prime.java
+++ prime.java
@@ -10,8 +10,19 @@ public class prime {
             System.out.println("first" +n+"prime numnbersss are:");
             System.out.println(2);
         }
-        for (int count =2; count<=n;){
-
+        for (int count =2; count<=n; ){
+             for (int j=2 ; j<=Math.sqrt(num);j++)
+             {
+                if (num%j==0){
+            status=0;
+            break;
+                }
+             }
+             if(status !=0)
+             {
+                System.out.println(num);
+                count++;
+             }
         }
     }
 
