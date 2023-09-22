c60af920c0f8df486a0d4061d8f5e139e8cf9c2d
Abhishek Yergude <112966485+abhii019@users.noreply.github.com>
2023-09-22 13:49:38 +0530

    .

--- invertstring1.java
+++ invertstring1.java
@@ -1,6 +1,6 @@
 public class invertstring1 {
     public static void main(String[] args) {
-        StringBuffer a = new StringBuffer("java priogramming is fun");
+        StringBuffer a = new StringBuffer(" literally java priogramming is fun");
         System.out.println(a.reverse());
     }
     }
