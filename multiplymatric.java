9452fdaedc2e35ab8748505910aaa7bd258e2605
Abhishek Yergude <112966485+abhii019@users.noreply.github.com>
2023-09-27 16:46:45 +0530

    .

---
+++ multiplymatric.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class multiplymatric {
+    public static void main(String[] args) {
+        int m,n,p,q,sum=0,c,d,k;
+
+    Scanner sc= new Scanner(System.in);
+    System.out.println("enetr the elements of row and column of the fiirst matrix"); 
+    }
+}
