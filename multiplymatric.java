8dea890db29ff06fe3e4ece7fa31dd2a3bc1e9a2
Abhishek Yergude <112966485+abhii019@users.noreply.github.com>
2023-09-28 16:07:52 +0530

    .

--- multiplymatric.java
+++ multiplymatric.java
@@ -5,5 +5,53 @@ public class multiplymatric {
 
     Scanner sc= new Scanner(System.in);
     System.out.println("enetr the elements of row and column of the fiirst matrix"); 
+
+    m= sc.nextInt();
+    n= sc.nextInt();
+
+    int first[][] = new int [m][n];
+    System.out.println("enter the elements of the first matrix");
+
+    for (c=0;c<m;c++)
+    for(d=0;d<n;d++)
+    first[c][d]=sc.nextInt();
+
+    System.out.println("enter the no of rows and columns of second matrix");
+    p=sc.nextInt();
+    q=sc.nextInt();
+
+    if(n!=p)
+    System.out.println("matrices with enetred orders cants be multiplied with each other");
+    else{
+        int second[][]=new int [p][q];
+        int multiply[][]= new int[m][q];
+
+        System.out.println("enter the elements of second matrices");
+        for (c=0; c<p;c++)
+        for (d=0; d<q;d++)
+        second[c][d]= sc.nextInt();
+
+        for (c=0;c<m;c++)
+        {
+            for(d=0;d<q;d++)
+            {
+                for (k=0;k<p;k++)
+                {
+                    sum = sum + first[c][k]*second[k][d]; 
+                }
+                multiply[c][d] = sum;
+                sum = 0;
+            }
+        }
+        System.out.println("Product of entered matrices:-");
+for ( c = 0 ; c < m ; c++ )
+{
+for ( d = 0 ; d < q ; d++ )
+System.out.print(multiply[c][d]+"\t");
+System.out.print("\n");
+}
+
+
+    }
     }
 }
