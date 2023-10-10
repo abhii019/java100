70c349748fa3f660c99b2878801d7a81dc1db2b1
Abhishek Yergude <112966485+abhii019@users.noreply.github.com>
2023-10-10 08:59:15 +0530

    .

--- linearsearch.java
+++ linearsearch.java
@@ -1,4 +1,28 @@
 import java.util.*;
+
+import javax.sql.rowset.spi.SyncResolver;
 public class linearsearch {
-    int c, n, search ,array[];
+    public static void main(String[] args) {
+     int c, n, search ,array[];
+
+    Scanner sc = new Scanner(System.in);
+    n= sc.nextInt();
+    array=new int[n];
+    System.out.println("Enter"+n+"integers");
+    for (c=0;c<n;c++)
+    array[ c]= sc.nextInt();
+
+    System.out.println("enetr value to find");
+    search = sc.nextInt();
+    for (c=0;c<n;c++)
+    {
+        if (array[c]==search)
+        {
+            System.out.println(search +"is present at location"+(c+1)+".");
+            break;
+        }
+        if (c==n)
+        System.out.println(search+"is not present in array");
+    }
+    }
 }
